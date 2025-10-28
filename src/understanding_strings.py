9a8a9dba01a239d0cfdc5b06aa6576b12a1f4768
Hectorpadro <hectorpadron2911@gmail.com>
2025-10-28 10:45:07 -0600

    Feat: understanding strings

---
+++ src/understanding_strings.py
@@ -0,0 +1,16 @@
+"""
+    String variables
+
+    Un string es de manera sencilla una serie de caracteres. 
+    En Python, todo lo que esté entre comillas simples (' ') o dobles (" ")
+    se considera un string.
+
+    Ejemplos: 
+        "Este es un string"
+        'Este tambien es un string'
+
+    'Le dije a un amigo "Python es mi lenguaje favorito"'
+    " El lenguaje 'Python' lleva el nombre de monty python "
+
+"""
+    
