6c7da25486b0e475d48dbc219638a8a15c4350b7
Hectorpadro <hectorpadron2911@gmail.com>
2025-11-20 16:16:02 -0600

    feas: understanding dictionari

---
+++ src/understanding_dictionarust.py
@@ -0,0 +1,22 @@
+# Empty Dictionary
+
+homer_0 = {"Color": "yellow", "bag": "homer-donut", "hair": "blue", "dress": "casual", "mom": False }
+print(homer_0)
+print(type(homer_0))
+
+
+marge = {"Color": "yellow", "bag": "homer-donut", "hair": "blue", "dress": "green", "mom": True }
+gun_0 = {"scar": "yellou_orange",  "headshot": 1.5}
+
+
+## Add element to dictionary
+print(homer_0)
+homer_0["x-position"] = 15
+homer_0["y-position"] = 25
+homer_0["z-position"] = 10
+print(homer_0)
+
+marge["x-position"] = 16
+marge["y-position"] = 26
+marge["z-position"] = 10 
+
--- src/understanding_if_statement_part2.py
+++ src/understanding_if_statement_part2.py
@@ -1,5 +1,4 @@
-
-
+"""
 try:
     age = int(input("Escribe tu edad: "))
 except:
@@ -7,7 +6,7 @@ except:
     print("Error, ingresaste un caracter no valido")
 
 
-# <>
+
 if age >=100:
     print("Tienes mas de un siglo")
 elif age >= 18 and age < 100:
@@ -18,3 +17,54 @@ else:
     print("Tuviste un error") 
 
 print("Hola Charly")
+"""
+
+
+
+try:
+    age = int(input("Escribe tu edad: "))
+except:
+    age = -1
+    print("Error, ingresaste un caracter no valido")
+
+
+if age <=4 and age>=0:
+    print("Entonces la entrada es gratuita")
+elif age < 18 and age > 4:
+     print("la entrada cuesta $200")
+elif age >= 18 :
+    print("Entonces la entrada cuesta $400") 
+elif age >=0 and age <=100:
+    print("Tienes mas de un siglo")
+else:
+    print("Tuviste un error") 
+
+
+#  Multiple if 
+guisos = ["deshebrada", "asado", "salsa verde", "pozole"]
+if "asado" in guisos:
+    print("Si hay asado")
+else: 
+    print("No hay asado")
+if "tamales" in guisos:
+    print("Hay tamales")
+else: 
+    print("No hay tamales")
+if "salsa verde" in guisos:
+    print("Si hay salsa verde")
+else: 
+    print("No hay salsa verde")
+
+# Utilizando varias listas
+
+guisos_disponinles = ["salsa verde", "deshebrada", "mole"]
+guisos_a_ordenar = ["deshebrada", "caldo de iguana"]
+
+print("¿Que guiso desea ordenar")
+for guiso in guisos_a_ordenar:
+    print(f"Deseo{deseo}")
+    if guisos_disponinles:
+        print(f"Si tenemos ese guiso{guiso}")
+    else:
+        print("No tenemos ese guiso")
+print("Realizando pedido....")
