acea158e1b7ff1c14e85b11c183f0f52a4d85c9d
Hectorpadro <hectorpadron2911@gmail.com>
2025-11-28 21:22:10 -0600

    feast

--- src/understanding_funcions.py
+++ src/understanding_funcions.py
@@ -16,6 +16,7 @@
     a christofer.
 
 """
+
 def gretting_christopher():
     """
         Funcion para saludar a una persona
@@ -25,7 +26,37 @@ def gretting_christopher():
         print("Hello christopher")
 
 gretting_christopher()
+
+
+# Ejemplo de una funcion que genere el nombre completo
+# de una persona y lo regrese 
+
+def create_full_name(first_name, middle_name,  last_name):
+    full_name = f"{first_name} {middle_name} {last_name}".title()
+    return full_name
     
+first_name = input("Dame tu primer nombre: ")
+middle_name = input("Dame tu segundo nombre: (Si no tiene segundi nombre dar enter) ")
+last_name = input("Dame tu apellido: ")
 
+# Argumentisos posicionales
+generated_fullname = create_full_name(
+    first_name.lower().strip(), 
+    middle_name.lower().strip(), 
+    last_name.lower().strip())
+print(generated_fullname)
 
+# Argumentos llave
+generated_fullname2 = create_full_name(
+    middle_name= middle_name,
+    first_name= first_name,
+    last_name= last_name
+)
+print(generated_fullname2)
 
+# args en funciones
+# kwargs en funciones
+# Manejo de datos (.txt, .csv, json, exel, works, pdf)
+# args via consola (sys)
+#
+#
